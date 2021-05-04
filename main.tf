852b314ef9f791f97d03119c4911414e185e8625
Mayank Sharma <mayanksh@vmware.com>
2021-05-04 05:34:56 +0000

    Added copyright and SPDX license Identifiers.

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache License 2.0
+
 terraform {
   required_providers {
     avi = {
--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache License 2.0
+
 output "servers" {
   value = avi_pool.lb_pool.servers
 }
